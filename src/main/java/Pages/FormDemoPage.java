98f342a37ad10a6bf0f4d1635b93aebd1cb84f92
iuditabigiu <iudita_zan@yahoo.com>
2023-11-20 17:54:45 +0000

    add country test

--- src/main/java/Pages/FormDemoPage.java
+++ src/main/java/Pages/FormDemoPage.java
@@ -1,2 +1,91 @@
-package Pages;public class FormDemoPage {
+package Pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class FormDemoPage extends BasePage {
+    public FormDemoPage(WebDriver driver) {
+        super(driver);
+        PageFactory.initElements(driver, this);
+
+    }
+
+    @FindBy(id = "name")
+    private WebElement nameField;
+
+    public void enterNameInNameField (String text) {
+        nameField.sendKeys(text);
+    }
+
+    @FindBy(id = "inputEmail4")
+    private WebElement emailField;
+
+    public void enterTextInEmailField(String text) {
+        emailField.sendKeys(text);
+    }
+
+    @FindBy(id = "inputPassword4")
+    private WebElement passwordField;
+
+    public void enterTextInPasswordField(String text) {
+        passwordField.sendKeys(text);
+    }
+    @FindBy(id = "company")
+    private WebElement companyField;
+
+    public void enterTextInCompanyField(String text) {
+     companyField.sendKeys(text);
+    }
+    @FindBy(id = "websitename")
+    private WebElement websiteField;
+
+    public void enterTextInWebsiteField(String text) {
+        websiteField.sendKeys(text);
+    }
+    @FindBy(css = "option[value=RO]")
+    private WebElement countryField;
+
+    public void selectCountryInCountryField(String text) {
+        countryField.click();
+    }
+    @FindBy(id = "inputCity")
+    private WebElement cityField;
+
+    public void enterTextInCityField(String text) {
+        cityField.sendKeys(text);
+    }
+    @FindBy(id = "inputAddress1")
+    private WebElement address1Field;
+
+    public void enterTextInAddress1Field(String text) {
+        address1Field.sendKeys(text);
+    }
+    @FindBy(id = "inputAddress2")
+    private WebElement address2Field;
+
+    public void enterTextInAddress2Field(String text) {
+        address2Field.sendKeys(text);
+    }
+    @FindBy(id = "inputState")
+    private WebElement stateField;
+
+    public void enterTextInStateField(String text) {
+        stateField.sendKeys(text);
+    }
+    @FindBy(id = "inputZip")
+    private WebElement zipCodeField;
+
+    public void enterTextInZipCodeField(String text) {
+        zipCodeField.sendKeys(text);
+    }
+    @FindBy(css=".text-right > .bg-lambda-900")
+    private WebElement submitButton;
+
+    public void ClickOnSubmitButton (String text) {
+        submitButton.sendKeys(text);
+    }
 }
+
--- src/test/java/FormDemoTests.java
+++ src/test/java/FormDemoTests.java
@@ -1,2 +1,108 @@
-package PACKAGE_NAME;public class FormDemoTests {
+import Pages.*;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import Pages.BasePage;
+import Pages.FormDemoPage;
+import org.testng.asserts.Assertion;
+
+public class FormDemoTests extends BasePage {
+
+    private FormDemoPage formDemoPage;
+    private final By formDemoPageLink = By.linkText("Input Form Submit");// Define the constant locator
+
+    @BeforeMethod
+    public void setUp() {
+        super.setUp();
+        formDemoPage = new FormDemoPage(driver);
+    }
+
+    @Test
+    public void enterYourNameInNameField() {
+        driver.findElement(formDemoPageLink).click();
+        formDemoPage.enterNameInNameField("Dita");
+
+
+        String actualResults = driver.findElement(By.id("name")).getText();
+        System.out.println(actualResults);
+        Assert.assertTrue(actualResults.contains(""));
+    }
+
+    @Test
+    public void enterYourEmailInEmailField() {
+        driver.findElement(formDemoPageLink).click();
+        formDemoPage.enterTextInEmailField("MyEmail@yahoo.com");
+
+        String actualResults = driver.findElement(By.id("inputEmail4")).getText();
+        System.out.println(actualResults);
+    }
+
+    @Test
+    public void enterAPasswordInPasswordField() {
+        driver.findElement(formDemoPageLink).click();
+        formDemoPage.enterTextInPasswordField("Password123");
+
+        String actualResults = driver.findElement(By.id("inputPassword4")).getText();
+        System.out.println(actualResults);}
+    @Test
+    public void enterCompanyNameInCompanyField() {
+        driver.findElement(formDemoPageLink).click();
+        formDemoPage.enterTextInCompanyField("My Company");
+
+        String actualResults = driver.findElement(By.id("company")).getText();
+        System.out.println(actualResults);}
+    @Test
+    public void enterWebsiteNameInWebsiteField() {
+        driver.findElement(formDemoPageLink).click();
+        formDemoPage.enterTextInWebsiteField("www.myWebsite.co.uk");
+
+        String actualResults = driver.findElement(By.id("websitename")).getText();
+        System.out.println(actualResults);}
+    @Test
+    public void selectCountryOnCountryField() {
+        driver.findElement(formDemoPageLink).click();
+        formDemoPage.selectCountryInCountryField("Romania");
+
+        String actualResults = driver.findElement(By.cssSelector("option[value=RO]")).getText();
+        System.out.println(actualResults);}
+   @Test
+   public void enterYourCityInCityField() {
+       driver.findElement(formDemoPageLink).click();
+       formDemoPage.enterTextInCityField("Timisoara");
+
+       String actualResults = driver.findElement(By.id("inputCity")).getText();
+       System.out.println(actualResults);}
+    @Test
+    public void enterYourAddressInAddress1Field() {
+        driver.findElement(formDemoPageLink).click();
+        formDemoPage.enterTextInAddress1Field("My Address");
+
+        String actualResults = driver.findElement(By.id("inputAddress1")).getText();
+        System.out.println(actualResults);}
+    @Test
+    public void enterYourAddressInAddress2Field() {
+        driver.findElement(formDemoPageLink).click();
+        formDemoPage.enterTextInAddress2Field("Bulgarus 108");
+
+        String actualResults = driver.findElement(By.id("inputAddress2")).getText();
+        System.out.println(actualResults);}
+    @Test
+    public void enterYourStareInStateField() {
+        driver.findElement(formDemoPageLink).click();
+        formDemoPage.enterTextInAddress2Field("Timis");
+
+        String actualResults = driver.findElement(By.id("inputState")).getText();
+        System.out.println(actualResults);}
+    @Test
+    public void enterYourZipCodeInZipCodeField() {
+        driver.findElement(formDemoPageLink).click();
+        formDemoPage.enterTextInAddress2Field("307241");
+
+        String actualResults = driver.findElement(By.id("inputZip")).getText();
+        System.out.println(actualResults);}
 }
+
+
